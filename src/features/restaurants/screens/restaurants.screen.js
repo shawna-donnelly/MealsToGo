eee50221a8df1735eb61536337109a0dba263f3e
shawna-donnelly <42677787+shawna-donnelly@users.noreply.github.com>
2023-08-26 20:53:44 -0600

    restaurants flatlist

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card/restaurant-info-card.component";
 import styled from "styled-components/native";
 
 export const RestaurantsScreen = () => {
   const [searchVal, setSearchVal] = useState("");
+  const [restaurants, setRestaurants] = useState([
+    {
+      name: "Some Restaurant",
+      icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+      photos: [
+        "https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      ],
+      address: "100 some random street",
+      isOpenNow: true,
+      rating: 4,
+      isClosedTemporarily: true,
+    },
+    { name: 2 },
+    { name: 3 },
+    { name: 4 },
+    { name: 5 },
+    { name: 6 },
+    { name: 7 },
+    { name: 8 },
+    { name: 9 },
+  ]);
+
   return (
     <SafeArea>
       <SearchContainer>
@@ -17,11 +39,19 @@ export const RestaurantsScreen = () => {
         />
       </SearchContainer>
       <List>
-        <RestaurantInfoCard />
+        <RestaurantList
+          data={restaurants}
+          renderItem={(item) => <RestaurantInfoCard />}
+          keyextractor={(item) => item.name}
+        />
       </List>
     </SafeArea>
   );
 };
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 16 },
+})``;
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
